a0ce25cddbf4edd0bca2ab0ea829978e1862dbb7
Mitja M. Zdouc <mmzdouc@gmail.com>
2026-03-10 21:09:21 +0100

    Updated onboarding docs

---
+++ config_setup/README.md
@@ -0,0 +1,9 @@
+# Instructions
+
+*Nota bene: assumes a fresh Ubuntu install*
+
+Update the respective files, e.g. by downloading them
+
+|File|Location|
+|----|--------|
+|config|~/.ssh/config|
--- software_setup/setup_student.sh
+++ software_setup/setup_student.sh
@@ -1,16 +1,9 @@
 #! /bin/bash
 set -euo pipefail
 
-
-# Vars general
-SSH_CONFIG="/home/student/.ssh/"
-BASHRC="/home/student/.bashrc"
-
 # Vars for direct downloads
 MZMINE="https://github.com/mzmine/mzmine/releases/download/v4.9.0/mzmine_4.9.0_amd64.deb"
 JETBRAINS="jetbrains-toolbox-3.3.1.75249"
-JAVA="openjdk-21-jdk"
-CYTOSCAPE="https://github.com/cytoscape/cytoscape/releases/download/3.10.4/Cytoscape_3_10_4_unix.sh"
 
 if [[ $EUID -ne 0 ]]; then
   echo "Please run as root (sudo ./setup.sh)"
@@ -72,34 +65,4 @@ echo "##########################################"
 echo "Installing MZmine4"
 curl -L "$MZMINE" -o /tmp/mzmine.deb
 apt install -y /tmp/mzmine.deb
-
-echo "##########################################"
-echo "Installing Cytoscape"
-apt install "$JAVA"
-curl -L "$CYTOSCAPE" -o /tmp/cytoscape.deb
-apt install -y /tmp/cytoscape.deb
-
-echo "##########################################"
-echo "Setting up .ssh directory"
-mkdir -p "$SSH_CONFIG"
-chown student:student "$SSH_CONFIG"
-chmod 700 "$SSH_CONFIG"
-cat <<EOF > "$SSH_CONFIG"config
-Host *
-	ForwardX11      yes
-	ForwardX11Trusted yes
-	ServerAliveInterval 30
-
-Host github.com
-	User git
-	IdentityFile "$SSH_CONFIG"id_rsa
-EOF
-
-echo "##########################################"
-echo "Setting up .bashrc file"
-cat <<EOF >> "$BASHRC"
-# enable an alias for python -> python3 (who is using python2 anyway nowadays)
-alias python="python3"
-EOF
-
 
